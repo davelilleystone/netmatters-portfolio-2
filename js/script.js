e91516224a27c13e6a377dc51226f0f4446d6396
Dave Lilleystone <davelilleystone@gmail.com>
2023-09-23 18:07:29 +0100

    complete sidebar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const hb = document.querySelector(".hamburger");
 const sb = document.querySelector(".sidebar");
-const cc = document.querySelector(".content-container");
 const breakpoint = 768;
 
 hb.addEventListener("click", () => {
@@ -8,21 +7,11 @@ hb.addEventListener("click", () => {
     sb.classList.toggle("sidebar--ms-open");
     hb.classList.toggle("hamburger--ms-open");
   }
-  if (innerWidth >= breakpoint) {
-    sb.classList.toggle("sidebar--hide");
-    hb.classList.toggle("hamburger--open");
-    cc.classList.toggle("content-container--hide-margin");
-  }
 });
 
 window.addEventListener("resize", () => {
   if (innerWidth >= breakpoint && sb.classList.contains("sidebar--ms-open")) {
     sb.classList.remove("sidebar--ms-open");
     hb.classList.remove("hamburger--ms-open");
-  }
-  if (innerWidth < breakpoint && sb.classList.contains("sidebar--hide")) {
-    sb.classList.remove("sidebar--hide");
-    hb.classList.remove("hamburger--open");
-    cc.classList.remove("content-container--hide-margin");
   }
 });
